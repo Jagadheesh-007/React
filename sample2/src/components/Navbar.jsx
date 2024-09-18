470bbabbd368ebc0229f012f8948305edcdd1de5
Jagadheesh-007 <jagadheesh2005@gmail.com>
2024-09-18 18:15:52 +0530

    navigation bar added

--- sample2/src/components/Navbar.jsx
+++ sample2/src/components/Navbar.jsx
@@ -1,14 +1,16 @@
+import {User2} from 'lucide-react'
 import React from 'react'
-
+import 'react-router-dom'
 const Navbar = () => {
     return (
         <>
-            <div className='w-full flex h-[4rem] text-white bg-green-500 border rounded-2xl font-serif shadow-lg'>
+            <div className='w-full flex h-[4rem] text-lg text-white bg-blue-600 font-serif shadow-md shadow-black'>
                     <div className='w-[50%] flex items-center ml-12 justify-start'>Jagadheesh B</div>
                 <div className='w-[50%] flex flex-row items-center pr-10 justify-end gap-10 list-none'>
                     <li>Home</li>
                     <li>Projects</li>
                     <li>Contact</li>
+                    <User2 />
                 </div>
             </div>
         </>
