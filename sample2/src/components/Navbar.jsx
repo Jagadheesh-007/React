eb21dd3d21321cd0be64c21c428be2fc0fc1d42a
Jagadheesh-007 <jagadheesh2005@gmail.com>
2024-09-24 11:15:36 +0530

    some codes are modified

--- sample2/src/components/Navbar.jsx
+++ sample2/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <>
             <div className='w-full flex h-[4rem] text-lg text-white bg-lime-500 font-serif shadow-md shadow-black'>
-                <div className='w-[50%] flex flex-row h-full items-center ml-10 justify-start text-xl'><div className="w-30 h-30 p-3 rounded-sm shadow shadow-black bg-lime-500">Jagadheesh B</div></div>
+                <div className='w-[50%] flex flex-row h-full items-center ml-10 justify-start text-xl'><div className="w-30 h-30 p-3 rounded-sm shadow shadow-black bg-lime-500">Portfolio</div></div>
 
                     <ul className='w-[50%] flex flex-row items-center pr-10 justify-end gap-10 list-none'>
                     <li><Link to={'/'} className='hover:bg-lime-400 shadow-black p-3 rounded-md hover:border-b-4 focus:'>Home</Link></li>
